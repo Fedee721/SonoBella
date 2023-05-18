dbae58d42237638daaba04da02e7b13d35c96071
Fedee721 <federicovega@gmail.com>
2023-05-18 20:57:52 -0300

    Creacion de proyecto Spring

---
+++ src/main/java/com/sonnobella/sonnobella/SonnobellaApplication.java
@@ -0,0 +1,13 @@
+package com.sonnobella.sonnobella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonnobellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonnobellaApplication.class, args);
+	}
+
+}
